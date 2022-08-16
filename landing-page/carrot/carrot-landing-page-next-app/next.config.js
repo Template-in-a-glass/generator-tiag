3b96fb774b30646cf241412ece1f0333f3ac7671
Vincent Delacourt <vincent.delacourt@joinhorizons.com>
2022-08-16 13:51:41 +0800

    Install Next Export Optimize Images

--- landing-page/carrot/carrot-landing-page-next-app/next.config.js
+++ landing-page/carrot/carrot-landing-page-next-app/next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
+
+const withExportImages = require('next-export-optimize-images');
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 }
 
-module.exports = nextConfig
+module.exports = withExportImages(nextConfig);
