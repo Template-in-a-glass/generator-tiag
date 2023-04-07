ba49f8bda92db9dc0b921685e330dc4c02b85833
Vincent DELACOURT <vdelacou@users.noreply.github.com>
2023-04-07 09:43:55 +0800

    Update create_landing-page-next-app.sh

--- landing-page/create_landing-page-next-app.sh
+++ landing-page/create_landing-page-next-app.sh
@@ -3,6 +3,12 @@
 npx -y create-next-app@latest --ts --eslint --experimental-app --src-dir --tailwind --use-npm --import-alias "@/*" $APP_NAME-landing-page-next-app
 cd $APP_NAME-landing-page-next-app
 
+# Init Conventional Commit
+npm install --save-dev @commitlint/cli @commitlint/config-conventional
+curl -o commitlint.config.ts https://raw.githubusercontent.com/Template-in-a-glass/template-landing-page-next-app/main/commitlint.config.ts
+git add .
+git commit -m "chore: Init Conventional Commit"
+
 # Install estlint and vscode plugin and settings
 mkdir .vscode
 curl -o .vscode/extensions.json https://raw.githubusercontent.com/Template-in-a-glass/template-landing-page-next-app/main/.vscode/extensions.json
@@ -10,11 +16,11 @@ curl -o .vscode/settings.json https://raw.githubusercontent.com/Template-in-a-gl
 
 npm install --save-dev @typescript-eslint/eslint-plugin @typescript-eslint/parser eslint eslint-config-airbnb eslint-import-resolver-typescript eslint-plugin-import eslint-plugin-jsx eslint-plugin-react eslint-plugin-react-hooks @types/node @types/react @types/react-dom eslint eslint-config-next typescript
 
-curl -o .eslintignore https://raw.githubusercontent.com/Template-in-a-glass/template-landing-page-next-app/main/.eslintignore
+# curl -o .eslintignore https://raw.githubusercontent.com/Template-in-a-glass/template-landing-page-next-app/main/.eslintignore
 curl -o .eslintrc.json https://raw.githubusercontent.com/Template-in-a-glass/template-landing-page-next-app/main/.eslintrc.json
 
 git add .
-git commit -m "Install estlint and vscode plugin and settings"
+git commit -m "chore: Install estlint and vscode plugin and settings"
 
 # Clean boilerplate
 
@@ -24,7 +30,7 @@ rm -f public/**
 rm -f src/app/globals.css src/app/page.module.css 
 
 git add .
-git commit -m "Clean boilerplate"
+git commit -m "chore: Clean boilerplate"
 
 # Update all libraries (can be dangerous)
 npm pkg set scripts.update-dependencies="npx -y npm-check-updates@latest -u"
@@ -32,13 +38,13 @@ npm run update-dependencies
 npm install
 
 git add .
-git commit -m "Update all libraries"
+git commit -m "chore: Update all libraries"
 
 # Download the code template
 curl https://codeload.github.com/Template-in-a-glass/template-landing-page-next-app/tar.gz/main | tar -xz --strip=1 template-landing-page-next-app-main/src/app
 
 git add .
-git commit -m "Download the code template"
+git commit -m "chore: Download the code template"
 
 # Install husky
 npm install husky --save-dev
@@ -48,12 +54,12 @@ npx -y husky@latest add .husky/pre-commit "npm run lint"
 npx -y husky@latest add .husky/pre-push "npm run lint && npm run build"
 
 git add .
-git commit -m "Install husky"
+git commit -m "chore: Install husky"
 
 # Set up git and project ready commit
-echo .dccache >> .gitignore
-git add .
-git commit -m "project ready"
+# echo .dccache >> .gitignore
+# git add .
+# git commit -m "chore: project ready"
 
 # Install Next SEO
 npm install next-sitemap --save
